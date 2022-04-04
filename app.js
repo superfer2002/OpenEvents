3cd00f60922268b991f9d3013a4f4d2cf0b4015e
superfer2002 <fporcel2002@gmail.com>
2022-04-04 18:29:53 +0200

    Node improvements

--- app.js
+++ app.js
@@ -7,4 +7,5 @@ app.use(express.static('dist'))
 app.listen(PORT, function (err) {
     if (err) console.log(err)
     console.log('Server listening on PORT', PORT)
+    console.log('http://localhost:3000/index.html')
 })
